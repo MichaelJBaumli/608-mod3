e0d587766049d807e6a303e7658d6bf956c91531
Michael Baumli <michaeljbaumli@gmail.com>
2021-05-20 17:06:48 -0500

    Fixing formatting and using the original intent of the program.

--- playing-craps.py
+++ playing-craps.py
@@ -15,8 +15,9 @@ def roll_dice():
 
 def display_dice(dice):
     die1,die2 = dice
+    '''
     print(f'Player rolled {die1} + {die2} = {sum(dice)}')
-
+    '''
 
 def play_craps():
     die_values = roll_dice()
@@ -33,8 +34,9 @@ def play_craps():
     else: #Remember point?
         game_status = "CONTINUE"
         my_point = sum_of_dice
+        '''
         print('Point is',my_point)
-
+        '''
     while game_status == 'CONTINUE':
         die_values = roll_dice()
         display_dice(die_values)
@@ -46,15 +48,20 @@ def play_craps():
             game_status = 'LOST'
 
     if game_status == 'WON':
+        '''
         print('Player wins')
+        '''
     else:
+        '''
         print('Player loses')
+        '''
     return game_status
 
 wins = 0
 loses = 0
+faces = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-for x in range(1000000):
+for x in range(6):
 
     game = play_craps()
     if game == 'WON':
@@ -62,6 +69,17 @@ for x in range(1000000):
     else:
         loses +=1
 
-print(f'You won {wins} times')
-print(f'You lost {loses} times')
+#print(f'You won {wins} times')
+#print(f'You lost {loses} times')
+
+for x in range(6000000):
+    die_values = roll_dice()
+    display_dice(die_values)
+    sum_of_dice = sum(die_values)
+    faces[sum_of_dice-1]+=1
+print("Face                 Frequency")
+for x in range(12):
+    x1 = x+1
+    print(str(x1).rjust(4),end=' ')
+    print(str(faces[x]).rjust(25))
 
