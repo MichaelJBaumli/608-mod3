272e7cd79cd6faee262ead57c295f8e6b7274707
Michael J Baumli <michaeljbaumli@gmail.com>
2021-05-19 21:37:51 -0500

    A night's work on two of the three programs built-in-min-max.py is complete playing-craps.py still needs work.  Probably modify it because I can.

---
+++ built-in-min-max.py
@@ -0,0 +1,28 @@
+#Program Name: built-in-min-max.py
+#Assignment Module 3
+#Class 44680 Block 44599 Section 01
+#Michael Baumli
+#Date: 20210519
+
+from random import randint
+
+q1 = [12,27,36]
+#If we get time, we will mess with this
+'''
+def q1gen():
+    q1.clear()
+    for x in (3):
+      y = randint(0,100)
+      q1.append(y)
+    return q1
+'''
+    
+
+minimum = min(q1)
+
+print("The Minimum is ", minimum)
+
+maximum = max(q1)
+
+print("The Maximum is ", maximum)
+
---
+++ playing-craps.py
@@ -0,0 +1,67 @@
+#Program Name: playing-craps.py
+#Assignment Module 3
+#Class 44680 Block 44599 Section 01
+#Michael Baumli
+#Date: 20210519
+
+
+#Copied from the Book and probably butchered for my needs
+import random
+
+def roll_dice():
+    die1 = random.randrange(1,7)
+    die2 = random.randrange(1,7)
+    return(die1,die2)
+
+def display_dice(dice):
+    die1,die2 = dice
+    print(f'Player rolled {die1} + {die2} = {sum(dice)}')
+
+
+def play_craps():
+    die_values = roll_dice()
+    display_dice(die_values)
+
+    # determine game status and point based on first roll
+    sum_of_dice = sum(die_values)
+
+
+    if sum_of_dice in (7,11): #Winning
+        game_status = "WON"
+    elif sum_of_dice in(2,3,12): #Losing
+        game_status = "LOST"
+    else: #Remember point?
+        game_status = "CONTINUE"
+        my_point = sum_of_dice
+        print('Point is',my_point)
+
+    while game_status == 'CONTINUE':
+        die_values = roll_dice()
+        display_dice(die_values)
+        sum_of_dice = sum(die_values)
+
+        if sum_of_dice == my_point:
+            game_status = 'WON'
+        elif sum_of_dice == 7:
+            game_status = 'LOST'
+
+    if game_status == 'WON':
+        print('Player wins')
+    else:
+        print('Player loses')
+    return game_status
+
+wins = 0
+loses = 0
+
+for x in range(1000000):
+
+    game = play_craps()
+    if game == 'WON':
+        wins +=1
+    else:
+        loses +=1
+
+print(f'You won {wins} times')
+print(f'You lost {loses} times')
+
