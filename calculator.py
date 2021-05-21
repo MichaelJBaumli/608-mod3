e268d884c00934a40f996f5afdb181e272365b4d
Michael J Baumli <michaeljbaumli@gmail.com>
2021-05-20 21:28:27 -0500

    Completed calculator.py.

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#Program Name: calculator.py
+#Assignment Module 3
+#Class 44680 Block 44599 Section 01
+#Michael Baumli
+#Date: 20210520
+
+import math
+
+print("Squareroot of 900 is ",math.sqrt(900))
+print("The floor of 9.2 is ",math.floor(9.2))
+print("The fabs of -10 is ",math.fabs(-10))
+
+
