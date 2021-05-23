d8c79edae05454fa58b3216e2daf9d75ff7515ea
Michael J Baumli <michaeljbaumli@gmail.com>
2021-05-23 09:05:38 -0500

    Updated custom-object.py to match the assignment with some formatting tweaks.

--- custom-object.py
+++ custom-object.py
@@ -4,6 +4,8 @@
 #Michael Baumli
 #Date: 20210521
 
+#This is a copy and paste from https://nwmissouri.instructure.com/courses/37830/pages/module-3-project-purchase-object-methods
+
 class Purchase(object):
     def __init__(self,amount):
         self.amount = amount
@@ -17,3 +19,24 @@ class Purchase(object):
         return self.amount * (1 + taxPercent/100 + tipPercent/100)
 
         
+#Create Purchase object given an amount
+purchase = Purchase(100.0)
+
+# Set the tax and tip percentages
+taxPercent = 7.5
+tipPercent = 20.0
+
+# Set the object to aclculate the tax and tip
+tax = purchase.calculateTax(taxPercent)
+tip = purchase.calculateTip(tipPercent)
+
+#Display some useful information
+
+print(f'Tax: {tax:0.2f}')
+print(f'Tip: {tip:0.2f}')
+print(f'Total: {purchase.calculateTotal(taxPercent,tipPercent):0.2f}')
+
+
+
+
+
